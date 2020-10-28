77a02ffe0512de9e2a86e441d1713bc6fb0d82d6
Arindam Dey <biplabhome@gmail.com>
2020-10-28 09:39:16 +0530

    primary solution

---
+++ OrgOps/OrgOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrgOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
